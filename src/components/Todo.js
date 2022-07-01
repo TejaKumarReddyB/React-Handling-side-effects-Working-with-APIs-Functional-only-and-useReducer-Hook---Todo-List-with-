3eeadfa5a81928c2af1f880b98962d3f9ce4e5a2
TejaKumarReddyB <98025617+TejaKumarReddyB@users.noreply.github.com>
2022-07-01 12:45:08 +0530

    Create Todo.js

---
+++ src/components/Todo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+const Todo = () => {
+    return (
+        <>
+        <h2 class="todo">Todo</h2>
+        {
+            state.map((cur)=>
+            <>
+            <div class="todo-title" key={id}>{cur}</div>
+            <button onClick={()=>dispatch({type:"delete"})}>del</button>
+            </>
+        }
+       
+        </>
+    )
+}
+
+
+export { Todo }
