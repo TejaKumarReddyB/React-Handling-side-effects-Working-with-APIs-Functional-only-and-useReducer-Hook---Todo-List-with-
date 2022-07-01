adba5d41a2c2029e391ab2e8e75cd205ba2981fd
TejaKumarReddyB <98025617+TejaKumarReddyB@users.noreply.github.com>
2022-07-01 12:44:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,19 @@
-import React, {Component, useState} from "react";
+import React, { useReducer } from 'react'
+import { todoReducer } from '../reducers/todo';
 import '../styles/App.css';
-
-const App = (props) => {
-  const [counter, setCounter] = useState(0);
-  const [data] = useState(props.slides);
-
+import { AddTodo } from './AddTodo';
+import { Todo } from './Todo';
+const App = () => {
+  const [state,dispatch] = useReducer(todoReducer,{/* Initial todo state array, could be an array for storing indivial todo objects */})
+  
  
   return (
-    <div>
-    <h1 data-testid="title">{data[counter].title}</h1>
-    <p data-testid="text">{data[counter].text}</p>
-    <button data-testid="button-next" disabled={counter === props.slides.length-1 ? true : false} onClick={() =>setCounter(counter+1)}>Next</button>
-    <button data-testid="button-prev" disabled={counter === 0 ? true : false} onClick={() => setCounter(counter-1)}>Prev</button>
-    <button data-testid="button-restart" onClick={() =>setCounter(0)} disabled={counter === 0 ? true : false}>Restart</button>
+    <div id="main">
+      <AddTodo />
+     {/* Render list of Todo Components here */}
     </div>
   )
 }
+
 
 export default App;
